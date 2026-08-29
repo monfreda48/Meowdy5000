cc0276a9ee3ec3057f5cc7bfc2f34cbe5ecb6f2f
monfreda48 <143972281+monfreda48@users.noreply.github.com>
2026-08-29 12:55:22 -0400

    Update release commit tag to ff17a3a

--- backend/app.py
+++ backend/app.py
@@ -358,7 +358,7 @@ def get_stats():
 
     return jsonify({"current": final_data, "history": history})
 
-CURRENT_VERSION_COMMIT = "138041e"
+CURRENT_VERSION_COMMIT = "ff17a3a"
 
 @app.route('/api/check-update')
 def check_update():
