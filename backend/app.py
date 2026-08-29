090df19e87759ea7a1cc69e9626f3e671c7972c1
monfreda48 <143972281+monfreda48@users.noreply.github.com>
2026-08-29 13:18:37 -0400

    Update release commit tag to 9b01a40

--- backend/app.py
+++ backend/app.py
@@ -358,7 +358,7 @@ def get_stats():
 
     return jsonify({"current": final_data, "history": history})
 
-CURRENT_VERSION_COMMIT = "d9a4810"
+CURRENT_VERSION_COMMIT = "9b01a40"
 
 def get_local_commit_sha():
     """Gets current local git commit SHA or falls back to static tag."""
