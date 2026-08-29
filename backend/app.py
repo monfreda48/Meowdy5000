6c4ace939dbae0c1713097b19e8ff1a850cf5287
monfreda48 <143972281+monfreda48@users.noreply.github.com>
2026-08-29 12:59:44 -0400

    Update release commit tag to c368149

--- backend/app.py
+++ backend/app.py
@@ -358,7 +358,7 @@ def get_stats():
 
     return jsonify({"current": final_data, "history": history})
 
-CURRENT_VERSION_COMMIT = "70c51a6"
+CURRENT_VERSION_COMMIT = "c368149"
 
 @app.route('/api/check-update')
 def check_update():
