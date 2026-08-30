b351f6ab4e8f7b743b08975c20886cc294c9d323
monfreda48 <143972281+monfreda48@users.noreply.github.com>
2026-08-29 20:43:42 -0400

    Update release commit tag to ff0b6ce

--- backend/app.py
+++ backend/app.py
@@ -369,7 +369,7 @@ def get_stats():
 
     return jsonify({"current": final_data, "history": history})
 
-CURRENT_VERSION_COMMIT = "d7f0676"
+CURRENT_VERSION_COMMIT = "ff0b6ce"
 
 def get_local_commit_sha():
     """Gets current local git commit SHA or falls back to static tag."""
